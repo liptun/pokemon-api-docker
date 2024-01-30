73d0639b3b997aa2c854e2cfec6bc3bb82fd055c
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2024-01-30 23:15:23 +0100

    cleanup

--- app/prisma/migrations/20240130221436_/migration.sql
+++ app/prisma/migrations/20240130221436_/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE `Trainer` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `name_jp` VARCHAR(191) NULL,
+    `userId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -25,12 +26,29 @@ CREATE TABLE `CatchedPokemon` (
     `name` VARCHAR(191) NULL,
     `name_jp` VARCHAR(191) NULL,
     `trainerId` INTEGER NOT NULL,
+    `userId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Trainer` ADD CONSTRAINT `Trainer_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `CatchedPokemon` ADD CONSTRAINT `CatchedPokemon_pokemonNo_fkey` FOREIGN KEY (`pokemonNo`) REFERENCES `Pokemon`(`no`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `CatchedPokemon` ADD CONSTRAINT `CatchedPokemon_trainerId_fkey` FOREIGN KEY (`trainerId`) REFERENCES `Trainer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CatchedPokemon` ADD CONSTRAINT `CatchedPokemon_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
