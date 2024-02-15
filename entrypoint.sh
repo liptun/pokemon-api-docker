a6c0410d796a0bd6ac7348ea3964d89393f43c02
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2024-02-15 18:58:08 +0100

    migrate and seed database on first run

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dbready=/database/.pokemon-db-ready
+
+echo "Starting entrypoint.sh"
+cd app
+
+if [ -e "$dbready" ]; then
+    echo "Database is already initialized"
+else
+    npx prisma migrate dev \
+    && npx prisma db seed \
+    && touch "$dbready" \
+    && echo "Initalization of database complete"
+fi
+
+
+if [ -e "$dbready" ]; then
+    npm i -g nodemon ts-node && npm i && nodemon --watch ./src --ext ts,yaml ./src/app.ts
+fi
+
