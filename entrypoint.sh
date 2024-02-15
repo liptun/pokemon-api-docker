80376f77f6167952ed751cc12105f56d726f4490
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2024-02-15 19:06:18 +0100

    retry migration if database connection failed

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,16 @@ cd app
 if [ -e "$dbready" ]; then
     echo "Database is already initialized"
 else
-    npx prisma migrate dev \
-    && npx prisma db seed \
+    while true; do
+        npx prisma migrate dev
+        if [ $? -eq 0 ]; then
+            break
+        fi
+        echo "Retrying connecting to database"
+        sleep 1
+    done
+
+    npx prisma db seed \
     && touch "$dbready" \
     && echo "Initalization of database complete"
 fi
