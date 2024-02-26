19821c723a9184adb1dd06598ac5968ebe4af579
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2024-02-26 13:53:29 +0100

    healthcheck on database service

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,22 +8,12 @@ cd app
 if [ -e "$dbready" ]; then
     echo "Database is already initialized"
 else
-    while true; do
-        npx prisma migrate dev
-        if [ $? -eq 0 ]; then
-            break
-        fi
-        echo "Retrying connecting to database"
-        sleep 1
-    done
-
-    npx prisma db seed \
+    npx prisma migrate dev \
+    && npx prisma db seed \
     && touch "$dbready" \
     && echo "Initalization of database complete"
 fi
 
 
-if [ -e "$dbready" ]; then
-    npm i -g nodemon ts-node && npm i && nodemon --watch ./src --ext ts,yaml ./src/app.ts
-fi
+npm i -g nodemon ts-node && npm i && nodemon --watch ./src --ext ts,yaml ./src/app.ts
 
